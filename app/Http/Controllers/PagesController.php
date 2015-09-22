428736334de73edaac7ad9e2f46ed7329e3d25f0
j-asis <j.asis@klab.com>
2015-09-22 10:48:33 +0800

    fixed initial files

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Routing\Controller as BaseController;
+
+class PagesController extends BaseController
+{
+    public function welcome()
+    {
+        return view('welcome');
+    }
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Hello there! Welcome to my site!</div>
             </div>
         </div>
     </body>
