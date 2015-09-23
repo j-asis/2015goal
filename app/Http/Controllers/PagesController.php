55bbeda4208fb6e2b481913789d153fbe8b0c215
j-asis <j.asis@klab.com>
2015-09-23 08:36:20 +0800

    removed redirect

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -13,9 +13,6 @@ class PagesController extends BaseController
 
     public function admin()
     {
-        if (!Auth::check()) {
-            return redirect(route('welcome'));
-        }
         return view('admin');
     }
 
