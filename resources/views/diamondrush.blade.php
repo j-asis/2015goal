c36496e8f5e53cb5e67a71306555d94e9ae17217
j-asis <j.asis@klab.com>
2015-09-24 10:56:41 +0800

    added tracker code in other games

--- resources/views/diamondrush.blade.php
+++ resources/views/diamondrush.blade.php
@@ -2,6 +2,17 @@
 <html>
 <head>
     <title>Diamond Rush by Rei</title>
+
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-67916027-1', 'auto');
+          ga('send', 'pageview');
+
+        </script>
 </head>
 <body>
 <iframe id="frame" src="http://chatplay-prototype.herokuapp.com/" frameborder="0"></iframe>
